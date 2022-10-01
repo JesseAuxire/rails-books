78cccba461fa01de385ab15111c0eb6a0cc7aac9
Jesse Auxire <auxire.jesse@hotmail.fr>
2022-10-01 16:50:21 +0200

    added seeds and index books

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'books#index'
   resources :books
 end
